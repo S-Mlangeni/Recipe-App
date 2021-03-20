bf8233db7b58c8ae5afb6333819f2a96185d0f2f
S-Mlangeni <smlangeni21@gmail.com>
2021-03-20 10:36:22 +0200

    update

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ function App() {
   server every time a new variable is added to the .env file (i.e. every time you set a new environment variable). */
   const [recipeLabel, setRecipeLabel] = useState([]);
   const [Search, setSearch] = useState("");
+  const [Loading, setLoading] = useState(false);
   
   const SearchFunc = (event) =>{
     setSearch(event.target.value);//"event.target" needs to be called in React.
   }
   
   const Data = async (event) => { //"event" parameter needs to be passed in since this function will be triggered by a user event.
+    setLoading(true);
     event.preventDefault(); //To prevent page from refreshing as form element is used. "e", which is short for event, can be used instead.
     const userquery = Search;
     //setSearch(""); //No need to empty seachbox. Hence, this line commented out.
@@ -23,28 +25,30 @@ function App() {
     //"async" is the corresponding keyword which allows asynchronous calls to be made. These keywords are commonly used when working with API/third-party systems.
     //Back ticks in the API url are used to allow for concatenation of the url with previously defined string variables - ES6 allows us to do this.
     const formatted_API_info = await API_info.json(); //The "json" method formats/cleans-up the collected data then presents it in a relatively readable way.
-    console.log(formatted_API_info.hits);
+    setLoading(false);
     setRecipeLabel(formatted_API_info.hits); //A State variable needs to be created then updated to transfer the fetched information inside from the function to the component (outside the function "Data"). This will allow the information to be read and returned by the component.
     console.log(recipeLabel);
+
   }
   
   return (
     <div className="App">
-     <h1>Food Recipes</h1>
-     <form>
-       <input type="text" className="searchbox" value={Search} onChange={SearchFunc}/>
-       <input type="submit" value="Search" className="searchbutton" onClick={Data} />
-     </form>
-     {recipeLabel.map((items)=>{
-       return(<div> <h2>{items.recipe.label}</h2>
-                    <img src={items.recipe.image} alt="" />
-                    <p>Ingredients:</p>
-                    {items.recipe.ingredients.map((ingred)=>{
-                      return (<ul><li>{ingred.text}</li></ul>)
-                    })}
-              </div>); //The "alt" attribute/prop in the image element is required by React. Otherwise a warning is displayed.
-     })}
-     
+      <h1>Food Recipes</h1>
+      <form>
+        <input type="text" className="searchbox" value={Search} onChange={SearchFunc}/>
+        <input type="submit" value="Search" className="searchbutton" onClick={Data} />
+      </form>
+      <div className={Loading ? "loader" : "loader_off"}></div>
+      {recipeLabel.map((items)=>{
+        return( <div className="recipe"> 
+                  <h2>{items.recipe.label}</h2>
+                  <img src={items.recipe.image} alt="" />
+                  <p>Ingredients:</p>
+                  {items.recipe.ingredients.map((ingred)=>{
+                    return (<ul><li>{ingred.text}</li></ul>)
+                  })}
+                </div>); //The "alt" attribute/prop in the image element is required by React. Otherwise a warning is displayed.
+      })}
     </div>
   );
 }
